c0a190189fd4c6cd1a3ab75f944defb78000d827
Lucas FB Lima <luucasfer94@gmail.com>
2023-03-30 21:33:22 -0300

    <feat>: Adicionando validações e mensagens de retorno

--- django-alura-adopet/adopet/quickstart/serializers.py
+++ django-alura-adopet/adopet/quickstart/serializers.py
@@ -5,17 +5,18 @@ from adopet.validators import *
 from django.http import JsonResponse
 from adopet.validators import validateEmail, validateName
 
-class TuctorSerializer(serializers.ModelSerializer):
+
+class TutorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tutor
         fields = ['id', 'name', 'email', 'phone', 'city', 'about']
-        #fields = '__all__'
+        
     def validate(self, data):
-        if not data:
-            raise serializers.ValidationError("Não há nenhum tutor cadastrado", status.HTTP_204_NO_CONTENT) 
         if not validateEmail(data['email']):
-            raise serializers.ValidationError("Este email não é válido", status.HTTP_400_BAD_REQUEST)
+            raise serializers.ValidationError("Este email não é válido", status=status.HTTP_400_BAD_REQUEST)
         if not validateName(data['name']):
-            raise serializers.ValidationError("O nome deve conter apenas letras", status.HTTP_400_BAD_REQUEST)
+            raise serializers.ValidationError("O nome deve conter apenas letras", status=status.HTTP_400_BAD_REQUEST)
         return data
+    
+
             
--- django-alura-adopet/adopet/quickstart/views.py
+++ django-alura-adopet/adopet/quickstart/views.py
@@ -6,5 +6,19 @@ from quickstart.serializers import *
 
 class TutorViewSet(viewsets.ModelViewSet):
     queryset = Tutor.objects.all()
-    serializer_class = TuctorSerializer 
+    serializer_class = TutorSerializer 
+    
+    def list(self, request):
+        tutors = Tutor.objects.all()
+        if tutors.count() == 0:
+            return Response({"mensagem": "Não há nenhum tutor cadastrado"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = TutorSerializer(tutors, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({"mensagem":"Tutor deletado com sucesso"}, status=status.HTTP_200_OK)
+    def perform_destroy(self, instance):
+        instance.delete()
  
